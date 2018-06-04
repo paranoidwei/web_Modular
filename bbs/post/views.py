8098c93bbe0edcaca8559c610665f2c4f65725b6
weiyulincode <1261667742@qq.com>
2018-06-05 01:11:09 +0800

    添加了文章处理功能

--- bbs/post/views.py
+++ bbs/post/views.py
@@ -1,17 +1,47 @@
-from django.shortcuts import render
+from math import ceil
 
-# Create your views here.
+from django.shortcuts import render, redirect
+
+from post.models import Post
+
+
 def create(request):
-    return render(request, "create.html", {})
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        post = Post.objects.create(title=title, content=content)
+        return redirect('/post/read/?post_id=%d' % post.id)
+    else:
+        return render(request, 'create.html')
+
 
 def edit(request):
-    return render(request, "edit.html", {})
+    if request.method == 'POST':
+        post_id = int(request.POST.get('post_id'))
+        post = Post.objects.get(id=post_id)
+
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('/post/read/?post_id=%d' % post.id)
+    else:
+        post_id = int(request.GET.get('post_id'))
+        post = Post.objects.get(id=post_id)
+        return render(request, 'edit.html', {'post': post})
+
 
 def read(request):
-    return render(request, "read.html", {})
+    post_id = int(request.GET.get('post_id'))
+    try:
+        post = Post.objects.get(id=post_id)
+        return render(request, 'read.html', {'post': post})
+    except Post.DoesNotExist:
+        return redirect('/')
 
 def post_list(request):
     return render(request, "post_list.html", {})
 
 def search(request):
-    return render(request, "search.html", {})
+    keyword = request.POST.get('keyword')
+    posts = Post.objects.filter(content__contains=keyword)
+    return render(request, 'search.html', {'posts': posts})
