546d2eae683aa33490fecf4ed77cff324a6a25dd
weiyulincode <1261667742@qq.com>
2018-06-04 21:20:15 +0800

    实现自定义多页显示数据

--- multipage/pages/views.py
+++ multipage/pages/views.py
@@ -1,20 +1,12 @@
-from django.shortcuts import HttpResponse, render
-from pages import models
-
-
-# Create your views here.
-
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
 from pages import models
-
-
-# Create your views here.
 
 
 class Pager(object):
     def __init__(self, current_page):
         self.current_page = int(current_page)
+
         # 把方法伪造成属性(1)
 
     @property
@@ -25,10 +17,99 @@ class Pager(object):
     def end(self):
         return self.current_page * 10
 
+    def page_str(self, all_item, base_url):
+        all_page, div = divmod(all_item, 10)
+
+        if div > 0:
+            all_page += 1
+
+        pager_list = []
+
+        # # 默认可以看到的页码11个
+        #
+        # start = self.current_page - 5
+        # end = self.current_page + 6
+        #
+        # # 把页面动态起来传入起始和结束
+        # for i in range(start, end):
+        #
+        #     # 判断是否为当前页
+        #     if i == self.current_page:
+        #         temp = '<a style="color:red;font-size:26px;padding: 5px" href="%s?page=%d">%d</a>' % (base_url,i,i)
+        #     else:
+        #         temp = '<a style="padding: 5px" href="%s?page=%d">%d</a>' % (base_url,i,i)
+        #
+        #     # 把标签拼接然后返回给前端
+        #     pager_str += temp
+
+        if all_page <= 11:
+            start = 1
+            end = all_page
+        else:
+            if self.current_page <= 6:
+                start = 1
+                end = 11 + 1
+            else:
+                start = self.current_page - 5
+                end = self.current_page + 6
+                if self.current_page + 6 > all_page:
+                    start = all_page - 10
+                    end = all_page + 1
+
+                    # 把页面动态起来传入起始和结束
+        for i in range(start, end):
+
+            # 判断是否为当前页
+            if i == self.current_page:
+                temp = '<a style="color:red;font-size:26px;padding: 5px" href="%s?page=%d">%d</a>' % (base_url, i, i)
+            else:
+                temp = '<a style="padding: 5px" href="%s?page=%d">%d</a>' % (base_url, i, i)
+
+                # 把标签拼接然后返回给前端
+            pager_list.append(temp)
+
+            # 上一页
+        if self.current_page > 1:
+            pre_page = '<a href="%s?page=%d">上一页</a>' % (base_url, self.current_page - 1)
+        else:
+            # javascript:void(0) 什么都不干
+            pre_page = '<a href="javascript:void(0);">上一页</a>'
+            # 下一页
+        if self.current_page >= all_page:
+            next_page = '<a href="javascript:void(0);">下一页</a>'
+        else:
+            next_page = '<a href="%s?page=%d">下一页</a>' % (base_url, self.current_page + 1)
+
+        pager_list.insert(0, pre_page)
+        pager_list.append(next_page)
+
+        return "".join(pager_list)
+
 
 def user_list(request):
+    # for i in range(500):
+    #     dic = {'username': 'name_%d' % i, 'age': i}
+    #     models.UserList.objects.create(**dic)
+    # return HttpResponse('OK')
     current_page = request.GET.get('page', 1)
     page_obj = Pager(current_page)
-    # 把方法改造成属性(2),这样在下面调用方法的时候就不需要加括号了
+    # 把方法改成属性(2), 这样在下面调用方法的时候就不需要加括号了
+    # print(current_page)
+    # current_page = int(current_page)
+    # start = (current_page-1)*10
+    # end = current_page*10
     result = models.UserList.objects.all()[page_obj.start:page_obj.end]
-    return render(request, 'page_list.html', {'result': result})
+    all_item = models.UserList.objects.all().count()
+    pager_str = page_obj.page_str(all_item, '/user_list/')
+    # all_page, div = divmod(all_item, 10)
+    #
+    # if div > 0:
+    #     all_page += 1
+    #
+    # pager_str = ""
+    # for i in range(1, all_page+1):
+    #     # 每次循环生成一个标签
+    #     temp = '<a href="pages/user_list/?page=%d">%d</a>' % (i, i,)
+    #     # 把标签拼接然后返回给前端
+    #     pager_str += temp
+    return render(request, 'page_list.html', {'result': result, 'pager_str': pager_str})
