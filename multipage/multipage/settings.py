fb086bf829ae53f2b1b746651d8e334ee4e1fd33
Why-Man <36983705+weiyulincode@users.noreply.github.com>
2018-06-04 21:51:57 +0800

    实现自定义多页显示数据功能模块
    * 添加pages模块,初步显示页面数据
    
    * 初步实现通过url显示多页页面数据
    
    * 实现自定义多页显示数据

--- multipage/multipage/settings.py
+++ multipage/multipage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qjo(-*^9m6=7^&l+#x3c4uhwe^6^86^ws^5t(3k0kyyt09x996'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'multipage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
