27bcb735aa03315b9694fde4d94fb813f342a079
weiyulincode <1261667742@qq.com>
2018-06-04 21:46:08 +0800

    Revert "Develop (#2)"
    This reverts commit 2ccd9ebabb4ffa511e3d595323eb11b2d0477372.

--- multipage/multipage/settings.py
+++ multipage/multipage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qjo(-*^9m6=7^&l+#x3c4uhwe^6^86^ws^5t(3k0kyyt09x996'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'multipage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- multipage/pages/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class UserList(models.Model):
-    username = models.CharField(max_length=32)
-    age = models.IntegerField()
